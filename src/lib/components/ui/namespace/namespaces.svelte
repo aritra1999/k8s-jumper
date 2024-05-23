f2885274bfce236e38cc9a650d8bbd24e90d7028
Aritra <aritra041999.dev@gmail.com>
2024-05-23 16:36:15 +0530

    Use loader component

--- src/lib/components/ui/namespace/namespaces.svelte
+++ src/lib/components/ui/namespace/namespaces.svelte
@@ -1,47 +1,52 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
 	import { Input } from '$lib/components/ui/input';
+	import Loader from '$lib/components/ui/loader/loader.svelte';
 	import { loadingStore, namespacesStore, resourcesStore } from '$lib/store';
 	import { ChevronRight, Search, X } from 'lucide-svelte';
 
-	export let namespaces: any[];
+	export let namespaces: any;
 	export let loadResources: any;
 	let showNamespaceSearch = false;
 </script>
 
-<div class="w-1/3 overflow-y-auto border-r p-2">
-	<div class="mb-2 flex items-center justify-between border-b-2 pb-2">
-		<div class="mr-2 w-full">
-			{#if showNamespaceSearch}
-				<Input type="text" class="w-full" />
-			{:else}
-				<h2 class="text-xl font-bold">Namespaces</h2>
-			{/if}
+<div class="h-full w-1/3 overflow-y-auto border-r p-2">
+	{#if $loadingStore.namespaces}
+		<div class="flex h-full items-center justify-center">
+			<Loader message="Loading namespaces" />
 		</div>
-		<div>
-			<Button
-				variant="secondary"
-				size="icon"
-				on:click={() => {
-					showNamespaceSearch = !showNamespaceSearch;
-				}}
-			>
+	{:else if namespaces}
+		<div class="mb-2 flex items-center justify-between border-b pb-2">
+			<div class="mr-2 w-full">
 				{#if showNamespaceSearch}
-					<X class="h-4 w-4" />
+					<Input type="text" class="w-full" />
 				{:else}
-					<Search class="h-4 w-4" />
+					<h2 class="text-xl font-bold">Namespaces</h2>
 				{/if}
-			</Button>
+			</div>
+			<div>
+				<Button
+					variant="secondary"
+					size="icon"
+					on:click={() => {
+						showNamespaceSearch = !showNamespaceSearch;
+					}}
+				>
+					{#if showNamespaceSearch}
+						<X class="h-4 w-4" />
+					{:else}
+						<Search class="h-4 w-4" />
+					{/if}
+				</Button>
+			</div>
 		</div>
-	</div>
-	{#if namespaces}
 		<div class="context space-y-2 pb-16">
 			{#each namespaces as namespace}
 				<Button
 					class="flex w-full items-center justify-between truncate"
+					variant={namespace.metadata.name === $resourcesStore.namespace ? 'secondary' : 'ghost'}
 					on:click={() => loadResources($namespacesStore.context, namespace.metadata.name)}
 					disabled={$loadingStore.namespaces || $loadingStore.resources}
-					variant={namespace.metadata.name === $resourcesStore.namespace ? 'secondary' : 'ghost'}
 				>
 					<span>{namespace.metadata.name}</span>
 					{#if namespace.metadata.name === $resourcesStore.namespace}
@@ -52,6 +57,3 @@
 		</div>
 	{/if}
 </div>
-
-<!-- disabled={$namespaces.loading || $resources.loading} -->
-<!-- variant={$namespaces.selected === context.name ? 'secondary' : 'ghost'} -->
--- src/lib/components/ui/resource/resources.svelte
+++ src/lib/components/ui/resource/resources.svelte
@@ -1,50 +1,54 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
 	import { Input } from '$lib/components/ui/input';
+	import Loader from '$lib/components/ui/loader/loader.svelte';
+	import { loadingStore } from '$lib/store';
 	import { addDots } from '$lib/utils';
 	import { Search, X } from 'lucide-svelte';
 
-	export let resources: any[];
+	export let resources: any;
 	export let type: string;
 	let showResourcesSearch = false;
 </script>
 
-<div class="w-1/3 overflow-y-auto border-r p-2">
-	<div class="mb-2 flex items-center justify-between border-b-2 pb-2">
-		<div class="mr-2 w-full">
-			{#if showResourcesSearch}
-				<Input type="text" class="w-full" />
-			{:else}
-				<h2 class="text-xl font-bold capitalize">{type}</h2>
-			{/if}
+<div class="w-1/3 overflow-y-auto p-2">
+	{#if $loadingStore.resources}
+		<div class="flex h-full items-center justify-center">
+			<Loader message="Loading resources" />
 		</div>
-		<div>
-			<Button
-				variant="secondary"
-				size="icon"
-				on:click={() => {
-					showResourcesSearch = !showResourcesSearch;
-				}}
-			>
+	{:else if resources}
+		<div class="mb-2 flex items-center justify-between border-b pb-2">
+			<div class="mr-2 w-full">
 				{#if showResourcesSearch}
-					<X class="h-4 w-4" />
+					<Input type="text" class="w-full" />
 				{:else}
-					<Search class="h-4 w-4" />
+					<h2 class="text-xl font-bold capitalize">{type}</h2>
 				{/if}
-			</Button>
+			</div>
+			<div>
+				<Button
+					variant="secondary"
+					size="icon"
+					on:click={() => {
+						showResourcesSearch = !showResourcesSearch;
+					}}
+				>
+					{#if showResourcesSearch}
+						<X class="h-4 w-4" />
+					{:else}
+						<Search class="h-4 w-4" />
+					{/if}
+				</Button>
+			</div>
 		</div>
-	</div>
-	<div class="context space-y-2 pb-16">
-		{#each resources as resource}
-			<Button variant="secondary" class="w-full justify-start">
-				<span use:addDots={30}>
-					{resource.metadata.name}
-					sdfsfsdfsdfsdfsfsdf
-				</span>
-			</Button>
-		{/each}
-	</div>
+		<div class="context space-y-2 pb-16">
+			{#each resources as resource}
+				<Button variant="ghost" class="flex w-full items-center justify-between truncate">
+					<span use:addDots={30}>
+						{resource.metadata.name}
+					</span>
+				</Button>
+			{/each}
+		</div>
+	{/if}
 </div>
-
-<!-- disabled={$namespaces.loading || $resources.loading} -->
-<!-- variant={$namespaces.selected === context.name ? 'secondary' : 'ghost'} -->
