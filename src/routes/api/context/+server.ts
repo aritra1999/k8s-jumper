04b6dc905051b8c88ee989db5e04a4af3528e6a0
Aritra <aritra041999.dev@gmail.com>
2024-05-30 22:54:25 +0530

    chore: Refactor API endpoints for namespace and context

--- src/routes/api/context/+server.ts
+++ src/routes/api/context/+server.ts
@@ -1,12 +1,6 @@
-import { cmd } from '$lib/commands';
+import { contexts } from '$lib/k8s';
 import { json } from '@sveltejs/kit';
 
-async function loadContexts() {
-	const config = (await cmd('kubectl config view -o json')) as string;
-	return JSON.parse(config).contexts;
-}
-
 export async function GET() {
-	const contexts = await loadContexts();
 	return json(contexts);
 }
