2d9513923ee7878960d6fe6f7ea93df102e54b4e
Aritra <aritra041999.dev@gmail.com>
2024-05-31 00:42:17 +0530

    shadcn: update button to use new font

--- src/lib/components/ui/button/button.svelte
+++ src/lib/components/ui/button/button.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-	import { Button as ButtonPrimitive } from "bits-ui";
-	import { cn } from "$lib/utils";
-	import { buttonVariants, type Props, type Events } from ".";
+	import { Button as ButtonPrimitive } from 'bits-ui';
+	import { cn } from '$lib/utils';
+	import { buttonVariants, type Props, type Events } from '.';
 
 	type $$Props = Props;
 	type $$Events = Events;
 
-	let className: $$Props["class"] = undefined;
-	export let variant: $$Props["variant"] = "default";
-	export let size: $$Props["size"] = "default";
-	export let builders: $$Props["builders"] = [];
+	let className: $$Props['class'] = undefined;
+	export let variant: $$Props['variant'] = 'default';
+	export let size: $$Props['size'] = 'default';
+	export let builders: $$Props['builders'] = [];
 	export { className as class };
 </script>
 
@@ -21,5 +21,15 @@
 	on:click
 	on:keydown
 >
-	<slot />
+	<span
+		class="absolute inset-0 h-full w-full translate-x-1 translate-y-1 transform bg-foreground transition duration-200 ease-out group-hover:-translate-x-0 group-hover:-translate-y-0"
+	>
+	</span>
+	<span
+		class="absolute inset-0 h-full w-full border-2 border-foreground bg-background group-hover:bg-foreground"
+	>
+	</span>
+	<span class="relative text-foreground group-hover:text-background">
+		<slot />
+	</span>
 </ButtonPrimitive.Root>
