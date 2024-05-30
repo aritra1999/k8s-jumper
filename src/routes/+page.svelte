f49da9ca330d09e413e6a20f37d924d72d7e6a22
Aritra <aritra041999.dev@gmail.com>
2024-05-31 00:42:55 +0530

    chore: add placeholder routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,10 @@
-<section class="flex h-screen w-full items-center justify-center">Welcome</section>
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<section class="flex h-screen w-full items-center justify-center">
+	<div>
+		<h2 class="mb-5">welcome to k8sx...</h2>
+		<Button>getting started</Button>
+	</div>
+</section>
