7b2c2b7c4075f8541624381687a0fc51b1766349
Aritra <aritra041999.dev@gmail.com>
2024-05-31 20:31:14 +0530

    feat: Refactor search functionality to use reusable component

--- src/lib/components/ui/namespace/namespaces.svelte
+++ src/lib/components/ui/namespace/namespaces.svelte
@@ -1,53 +1,24 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button';
-	import { Input } from '$lib/components/ui/input';
 	import List from '$lib/components/ui/k8s/list.svelte';
+	import Search from '$lib/components/ui/k8s/search.svelte';
 	import Loader from '$lib/components/ui/loader/loader.svelte';
 	import { loadingStore, namespacesStore, resourcesStore } from '$lib/store';
-	import { filterItemListBySearchString } from '$lib/utils';
-	import { Search, X } from 'lucide-svelte';
 
 	export let namespaces: any;
 	export let loadResources: any;
-	let searchFor = '';
-	let toggleSearch = false;
 
-	$: filteredItems = filterItemListBySearchString(namespaces, searchFor, 'metadata.name');
+	let filteredItems = namespaces;
 </script>
 
 <div class="h-full w-1/3 border-r-2 border-foreground">
+	<Search items={namespaces} bind:filteredItems selector="metadata.name" title="namespaces" />
 	{#if $loadingStore.namespaces}
 		<div class="flex h-full items-center justify-center">
 			<Loader message="Loading namespaces" />
 		</div>
 	{:else if filteredItems}
-		<div class="flex items-center justify-between border-b-2 border-foreground p-2.5">
-			<div class="mr-2 w-full">
-				{#if toggleSearch}
-					<Input type="text" class="w-full" bind:value={searchFor} />
-				{:else}
-					<h4 class="pl-2">namespaces</h4>
-				{/if}
-			</div>
-			<div>
-				<Button
-					variant="secondary"
-					size="icon"
-					on:click={() => {
-						toggleSearch = !toggleSearch;
-						filteredItems = namespaces;
-					}}
-				>
-					{#if toggleSearch}
-						<X class="h-4 w-4" />
-					{:else}
-						<Search class="h-4 w-4" />
-					{/if}
-				</Button>
-			</div>
-		</div>
 		<List
-			items={filteredItems}
+			bind:items={filteredItems}
 			buttonConfig={{
 				displayName: 'metadata.name',
 				disabled: $loadingStore.namespaces || $loadingStore.resources,
--- src/lib/components/ui/resource/resources.svelte
+++ src/lib/components/ui/resource/resources.svelte
@@ -1,51 +1,22 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button';
-	import { Input } from '$lib/components/ui/input';
 	import List from '$lib/components/ui/k8s/list.svelte';
+	import Search from '$lib/components/ui/k8s/search.svelte';
 	import Loader from '$lib/components/ui/loader/loader.svelte';
 	import { loadingStore } from '$lib/store';
-	import { addDots, filterItemListBySearchString } from '$lib/utils';
-	import { Search, X } from 'lucide-svelte';
 
 	export let resources: any;
 	export let type: string;
-	let searchFor: string = '';
-	let showResourcesSearch = false;
 
-	$: filteredItems = filterItemListBySearchString(resources, searchFor, 'metadata.name');
+	let filteredItems = resources;
 </script>
 
 <div class="h-full w-1/3">
+	<Search items={resources} bind:filteredItems selector="metadata.name" title={type} />
 	{#if $loadingStore.resources}
 		<div class="flex h-full items-center justify-center">
 			<Loader message="Loading resources" />
 		</div>
 	{:else if filteredItems}
-		<div class="flex items-center justify-between border-b-2 border-foreground p-2.5">
-			<div class="mr-2 w-full">
-				{#if showResourcesSearch}
-					<Input type="text" class="w-full" bind:value={searchFor} />
-				{:else}
-					<h4 class="pl-2">{type}</h4>
-				{/if}
-			</div>
-			<div>
-				<Button
-					variant="secondary"
-					size="icon"
-					on:click={() => {
-						showResourcesSearch = !showResourcesSearch;
-						filteredItems = resources;
-					}}
-				>
-					{#if showResourcesSearch}
-						<X class="h-4 w-4" />
-					{:else}
-						<Search class="h-4 w-4" />
-					{/if}
-				</Button>
-			</div>
-		</div>
 		<List
 			items={filteredItems}
 			buttonConfig={{
