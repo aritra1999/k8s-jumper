d6f6bca8e192f230664a448be527d0b886f0daf1
Aritra <aritra041999.dev@gmail.com>
2024-05-31 10:45:38 +0530

    feat: Update API routes to use k8sClient and k8sApi

--- src/routes/api/context/+server.ts
+++ src/routes/api/context/+server.ts
@@ -1,6 +1,7 @@
-import { contexts } from '$lib/k8s';
+import { k8sClient } from '$lib/k8s';
 import { json } from '@sveltejs/kit';
 
 export async function GET() {
+	const contexts = await k8sClient.getContexts();
 	return json(contexts);
 }
