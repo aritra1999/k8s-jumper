bef814ba7fb477a2e24e0c0ca17160a7a37c95f8
Aritra <aritra041999.dev@gmail.com>
2024-05-31 10:44:16 +0530

    feat: Add K8s list component and use it in resources and contexts components

--- src/lib/components/ui/context/contexts.svelte
+++ src/lib/components/ui/context/contexts.svelte
@@ -1,21 +1,26 @@
 <script lang="ts">
+	import { X, Search } from 'lucide-svelte';
 	import { Button } from '$lib/components/ui/button';
 	import { Input } from '$lib/components/ui/input';
 	import { loadingStore, namespacesStore } from '$lib/store';
-	import { ChevronRight, Search, X } from 'lucide-svelte';
+	import List from '$lib/components/ui/k8s/list.svelte';
+	import { filterItemListBySearchString } from '$lib/utils';
 
 	export let contexts: any[];
 	export let loadNamespaces: any;
+	let searchFor = '';
 	let showContextSearch = false;
+
+	$: filteredContexts = filterItemListBySearchString(contexts, searchFor, 'name');
 </script>
 
-<div class="w-1/3 overflow-y-auto border-r p-2">
-	<div class="mb-2 flex items-center justify-between border-b-2 pb-2">
+<div class="w-1/3 border-r-2 border-foreground">
+	<div class="flex items-center justify-between border-b-2 border-foreground p-2.5">
 		<div class="mr-2 w-full">
 			{#if showContextSearch}
-				<Input type="text" class="w-full" />
+				<Input type="text" class="w-full" bind:value={searchFor} />
 			{:else}
-				<h2 class="text-xl font-bold">Contexts</h2>
+				<h4 class="pl-2">contexts</h4>
 			{/if}
 		</div>
 		<div>
@@ -24,6 +29,7 @@
 				size="icon"
 				on:click={() => {
 					showContextSearch = !showContextSearch;
+					filteredContexts = contexts;
 				}}
 			>
 				{#if showContextSearch}
@@ -34,21 +40,15 @@
 			</Button>
 		</div>
 	</div>
-	{#if contexts}
-		<div class="context space-y-2 pb-16">
-			{#each contexts as context}
-				<Button
-					class="flex w-full items-center justify-between truncate"
-					on:click={() => loadNamespaces(context.name)}
-					disabled={$loadingStore.namespaces || $loadingStore.resources}
-					variant={context.name === $namespacesStore.context ? 'secondary' : 'ghost'}
-				>
-					<span>{context.name}</span>
-					{#if context.name === $namespacesStore.context}
-						<ChevronRight class="h-4 w-4" />
-					{/if}
-				</Button>
-			{/each}
-		</div>
+	{#if filteredContexts}
+		<List
+			items={filteredContexts}
+			buttonConfig={{
+				displayName: 'name',
+				disabled: $loadingStore.namespaces || $loadingStore.resources,
+				selectedItemName: $namespacesStore.context,
+				fnOnClick: loadNamespaces
+			}}
+		/>
 	{/if}
 </div>
