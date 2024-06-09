631b4a2338e7a6ae0586c3cbee73d22885221a51
Aritra <aritra041999.dev@gmail.com>
2024-06-09 22:21:38 +0530

    feat: add new db output component for rendering response

---
+++ src/lib/components/ui/k8s/db-output.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import * as Table from '$lib/components/ui/table/index.js';
+
+	export let items;
+</script>
+
+<Table.Root>
+	<Table.Header class="h-16 border-b-2 border-foreground">
+		<Table.Row>
+			<Table.Head class="w-[10px]">index</Table.Head>
+			<Table.Head>Name</Table.Head>
+			<Table.Head>Status</Table.Head>
+			<Table.Head>Method</Table.Head>
+			<Table.Head class="text-right">Amount</Table.Head>
+		</Table.Row>
+	</Table.Header>
+	<Table.Body>
+		{#each items as invoice, i (i)}
+			<Table.Row>
+				<Table.Cell>{i}</Table.Cell>
+				<Table.Cell class="font-medium">{invoice.invoice}</Table.Cell>
+				<Table.Cell>{invoice.paymentStatus}</Table.Cell>
+				<Table.Cell>{invoice.paymentMethod}</Table.Cell>
+				<Table.Cell class="text-right">{invoice.totalAmount}</Table.Cell>
+			</Table.Row>
+		{/each}
+	</Table.Body>
+</Table.Root>
